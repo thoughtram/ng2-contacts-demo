d06f66d64e3aabef1adc85fbfef252172d8130d9
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-10-06 21:20:49 +0200

    step 6: create ContactDetailComponent

--- app/components/contacts-list/contacts-list-component.ts
+++ app/components/contacts-list/contacts-list-component.ts
@@ -1,10 +1,12 @@
 import {Component} from 'angular2/angular2';
 import {Contact} from '../../models/contact';
 import {ContactsService} from '../../common/contacts-service';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
   selector: 'contacts-list-component',
-  templateUrl: './components/contacts-list/contacts-list-component.html'
+  templateUrl: './components/contacts-list/contacts-list-component.html',
+  directives: [ROUTER_DIRECTIVES]
 })
 export class ContactsListComponent {
   contacts: Array<Contact>;
--- app/contacts-app.ts
+++ app/contacts-app.ts
@@ -2,6 +2,7 @@ import {Component, bootstrap} from 'angular2/angular2';
 import {Contact} from './models/contact';
 import {ContactsService} from './common/contacts-service';
 import {ContactsListComponent} from './components/contacts-list/contacts-list-component';
+import {ContactDetailComponent} from './components/contact-detail/contact-detail-component';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component({
@@ -19,7 +20,8 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router'
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-  { path: '/', component: ContactsListComponent }
+  { path: '/', component: ContactsListComponent, as: 'ContactsList' },
+  { path: '/contact/:id', component: ContactDetailComponent, as: 'ContactDetail' },
 ])
 class ContactsApp {
 }
